5ede3b1045ff6610be9558cbbfd515ec4faeb662
shawnw <shawnw@speakeasy.org>
2015-12-04 00:49:30 -0800

    Day 4 part 2
    Support arbitrary length 0 prefixes.

--- day04.cc
+++ day04.cc
@@ -1,29 +1,44 @@
+// Takes one argument, the length of the leading 0s needed in the md5 hash.
+
 #include <iostream>
 #include <string>
 #include "md5.h"
 
-bool okay_hash(const std::string &hash) {
-	const static std::string leading_zeros {"00000"};
-	return hash.compare(0, 5, leading_zeros) == 0;
-}
+class compare_hash {
+	private:
+		std::string prefix;
+	public:
+		explicit compare_hash(int n) : prefix(n, '0') {}
+		bool matches(const std::string hash) {
+			return hash.compare(0, prefix.length(), prefix) == 0;
+		}
+};
 
 std::string make_secret_key(const std::string &seed, int n) {
 	return seed + std::to_string(n);
 }
 
-int main(void) {
+int main(int argc, char ** argv) {
 	std::string seed;
 	int magic;
+	
+	if (argc != 2) {
+		std::cerr << "Missing prefix length argument.\n";
+		return 1;
+	}
+	
+	compare_hash test(std::stoi(argv[1]));
 	
 	std::getline(std::cin, seed);
 	
 	for (magic = 1; true; magic += 1) {
 		const std::string key = make_secret_key(seed, magic);
 		const std::string hashed_key = md5(key);
-		if (okay_hash(hashed_key)) {
-			std::cout << magic << '\n';
+		if (test.matches(hashed_key)) {
+			std::cout << '\n' << magic << '\n';
 			break;
-		}
+		} else if (magic % 10000 == 0)
+			std::cout << '.';
 	}
 	return 0;
 }
