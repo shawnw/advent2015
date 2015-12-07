b2f76bf9467376fc3f6e112ce63233657509495d
shawnw <shawnw@speakeasy.org>
2015-12-06 21:11:25 -0800

    Day 05
    Day 05, part 2 - More naughty bits.

--- day05.cc
+++ day05.cc
@@ -2,32 +2,39 @@
 #include <string>
 #include <regex>
 
-bool isnice(const std::string &word) {
+bool isnice_part1(const std::string &word) {
 	static std::regex naughty_sequences {"ab|cd|pq|xy"};
 	static std::regex three_vowels { "[aeiou].*[aeiou].*[aeiou]" };
 	static std::regex duplicate_letter { "(.)\\1" };
 	
 	if (!std::regex_search(word, three_vowels))
 		return false;
-	
 	if (!std::regex_search(word, duplicate_letter))
 		return false;
-
 	return !std::regex_search(word, naughty_sequences);
+}
+
+bool isnice_part2(const std::string &word) {
+	static std::regex repeating_pair { "(..).*\\1" };
+	static std::regex repeating_single { "(.).\\1" };
+	
+	if (!std::regex_search(word, repeating_pair)) 
+		return false;
+	return std::regex_search(word, repeating_single);
 }
 
 int main(void) {
 	std::string word;
-	int nice = 0;
+	int nice_part1 = 0;
+	int nice_part2 = 0;
 	
 	while (std::getline(std::cin, word)) {
-		if (isnice(word)) {
-			//std::cout << "'" << word << "' is nice.\n";
-			nice += 1;
-		} else {
-			//std::cout << "'" << word << "' is naughty!\n";
-		}
+		if (isnice_part1(word))
+			nice_part1 += 1;
+		if (isnice_part2(word)) 
+			nice_part2 += 1;
 	}
-	std::cout << nice << '\n';
+	std::cout << "Part 1 nice count: " << nice_part1 << '\n';
+	std::cout << "Part 2 nice count: " << nice_part2 << '\n';
 	return 0;
 }
