5a5d5bf5d4f3b8bb641ee64ed7e422bb103b57f8
shawnw <shawnw@speakeasy.org>
2015-12-08 13:25:04 -0800

    Day 07 tweaks
    Some optimizations.

--- day07.cc
+++ day07.cc
@@ -1,31 +1,23 @@
+// Compile with: g++ -O -std=c++11 -o day07 day07.cc
+// Run as: ./day07 < day07.txt
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <regex>
 #include <string>
 #include <algorithm>
 #include <cctype>
+#include <cstdint>
 
 enum OPS {
 	OP_ERR, OP_AND, OP_OR, OP_NOT, OP_LSHIFT, OP_RSHIFT, OP_STORE 
 };
 
-using stype = unsigned short;
-
-struct gate {
-	OPS op;
-	std::string lhs;
-	std::string rhs;
-	bool memoized;
-	stype val;
-};
-
-using logic = std::map<std::string, gate>;
-
-bool isnumber(const std::string &n) {
+inline bool isnumber(const std::string &n) {
 	return std::all_of(n.begin(), n.end(), ::isdigit);
 }
 
-OPS stoop(const std::string s) {
+OPS stoop(const std::string &s) {
 	if (s == "AND")
 		return OP_AND;
 	else if (s == "OR")
@@ -38,45 +30,82 @@ OPS stoop(const std::string s) {
 		return OP_ERR;
 }
 
-stype solve_for(logic &, const std::string &);
+struct gate {
+	OPS op;
+	std::string lhs;
+	std::string rhs;
+	std::uint16_t rhsi;
+	bool memoized;
+	std::uint16_t val;
+	gate(void) : op(OP_ERR), lhs(""), rhs(""), rhsi(0), memoized(false), val(0) {}
+};
 
-stype solve_wire(logic &circuit, const std::string &num_or_wire) {
+std::ostream& operator<<(std::ostream &out, const gate &g) {
+	if (g.op == OP_STORE) {
+		out << g.lhs;
+	} else if (g.op == OP_NOT) {
+		out << "NOT " << g.lhs;
+	} else if (g.op == OP_AND) {
+		out << g.lhs << " AND " << g.rhs;
+	} else if (g.op == OP_OR) {
+		out << g.lhs << " OR " << g.rhs;
+	} else if (g.op == OP_LSHIFT) {
+		out << g.lhs << " LSHIFT " << g.rhs;
+	} else if (g.op == OP_RSHIFT) {
+		out << g.lhs << " RSHIFT " << g.rhs;
+	}
+	return out;
+}
+
+class logic {
+		std::unordered_map<std::string, gate> circuit;
+		std::uint16_t output_of(const std::string &);
+	public:
+		std::uint16_t find_output(const std::string &);
+		void dump_outputs(void);
+		void dump_ops(void);
+		void reset(void);
+		decltype(circuit)::size_type size(void) { return circuit.size(); }
+		void add(const std::string &wire, gate g) { circuit[wire] = g; }
+		gate& find_gate(const std::string &wire) { return circuit[wire]; }
+		int count_memoized(void);
+};	
+
+std::uint16_t logic::output_of(const std::string &num_or_wire) {
 	if (isnumber(num_or_wire))
 		return std::stoi(num_or_wire);
 	else
-		return solve_for(circuit, num_or_wire);
+		return find_output(num_or_wire);
 }
 
-stype solve_for(logic &circuit, const std::string &wire) {
-	
-	if (circuit.find(wire) == circuit.end()) {
-		std::cerr << "Unknown wire '" << wire << "'\n";
-		return 0;
-	}
-	
+std::uint16_t logic::find_output(const std::string &wire) {
 	gate &g = circuit[wire];
-	if (g.memoized) {
+	
+	if (1 && g.memoized) {
 		return g.val;
 	} else {
-		stype r;
+		std::uint16_t r = 0;
 		switch (g.op) {
+			case OP_ERR:
+				std::cerr << "Unknown gate operation for wire " << wire << '\n';
+				return 0;
 			case OP_STORE:
-				r = solve_wire(circuit, g.lhs);
+				r = output_of(g.lhs);
 				break;
 			case OP_AND:
-				r = solve_wire(circuit, g.lhs) & solve_wire(circuit, g.rhs);
+				r = output_of(g.lhs) & output_of(g.rhs);
 				break;
 			case OP_OR:
-				r = solve_wire(circuit, g.lhs) | solve_wire(circuit, g.rhs);
+				r = output_of(g.lhs) | output_of(g.rhs);
 				break;
 			case OP_LSHIFT:
-				r = solve_wire(circuit, g.lhs) << solve_wire(circuit, g.rhs);
+				r = output_of(g.lhs) << g.rhsi;
 				break;
 			case OP_RSHIFT:
-				r = solve_wire(circuit, g.lhs) >> solve_wire(circuit, g.rhs);
+				r = output_of(g.lhs) >> g.rhsi;
 				break;
 			case OP_NOT:
-				r = ~solve_wire(circuit, g.lhs);
+				r = ~output_of(g.lhs);
 				break;
 		}
 		g.memoized = true;
@@ -85,7 +114,25 @@ stype solve_for(logic &circuit, const std::string &wire) {
 	}
 }
 
-void reset_logic(logic &circuit) {
+int logic::count_memoized(void) {
+	int m = 0;
+	for (const auto &wires : circuit) 
+		if (wires.second.memoized)
+			m += 1;
+	return m;
+}
+
+void logic::dump_outputs(void) {
+	for (auto &wires : circuit)
+		std::cout << wires.first << ": " << find_output(wires.first) << '\n';
+}
+
+void logic::dump_ops(void) {
+	for (const auto &wires : circuit) 
+		std::cout << wires.second << " -> " << wires.first << '\n';
+}
+
+void logic::reset(void) {
 	for (auto &gate : circuit)
 		gate.second.memoized = false;
 }
@@ -93,52 +140,52 @@ void reset_logic(logic &circuit) {
 int main(void) {
 	std::string command;
 	logic circuit;
-    std::regex assign_op{ "(\\w+) -> (\\w+)" };
+	std::regex assign_op{ "(\\w+) -> (\\w+)" };
 	std::regex not_op{ "NOT (\\w+) -> (\\w+)" };
 	std::regex binary_op{ "(\\w+) (AND|OR|LSHIFT|RSHIFT) (\\w+) -> (\\w+)" };
 	
 	while (std::getline(std::cin, command)) {
 		std::smatch fields;
 		gate g;
-		g.memoized = false;
-		g.val = 0;
 		if (std::regex_match(command, fields, assign_op)) {
 			g.op = OP_STORE;
 			g.lhs = fields[1];
-			g.rhs = "";
-			circuit[fields[2]] = g;
+			circuit.add(fields[2], g);
 		} else if (std::regex_match(command, fields, not_op)) {
 			g.op = OP_NOT;
 			g.lhs = fields[1];
-			g.rhs = "";
-			circuit[fields[2]] = g;
+			circuit.add(fields[2], g);
 		} else if (std::regex_match(command, fields, binary_op)) {
 			g.op = stoop(fields[2]);
 			g.lhs = fields[1];
 			g.rhs = fields[3];
-			circuit[fields[4]] = g;
+			if (isnumber(fields[3]))
+				g.rhsi = std::stoi(fields[3]);
+			circuit.add(fields[4], g);
 		} else {
 			std::cerr << "Unknown gate: " << command << '\n';
 		}
 	}
 	
-	std::cout << "There are a total of " << circuit.size() << " gates.\n";
+	std::cout << "There are a total of " << circuit.size() << " wires.\n";
 	
-	for (auto &wires : circuit)
-		std::cout << wires.first << ": " << solve_for(circuit, wires.first) << '\n';
-	
-	stype a = solve_for(circuit, "a");
+	std::uint16_t a = circuit.find_output("a");
 	std::cout << "Initial value of a: " << a << '\n';
 	
-	reset_logic(circuit);
+	std::cout << "Used " << circuit.count_memoized() << " wires.\n";
+	
+	circuit.reset();
 	gate b;
 	b.op = OP_STORE;
+	b.lhs = std::to_string(a);
 	b.memoized = true;
 	b.val = a;
-	circuit["b"] = b;
+	circuit.add("b", b);
 	
-	a = solve_for(circuit, "a");
-	std::cout << "New value of a: " << a;
+	a = circuit.find_output("a");
+	std::cout << "New value of a: " << a << '\n';
+	
+	//circuit.dump_ops();
 	
 	return 0;
 }
